f015683bb3bdec0400cab4a64c0a883e30540dcd
Artemis Tosini <me@artem.ist>
2020-11-06 22:00:41 +0000

    Disable NetworkManager for starlight, network is broken now

--- sets/fpga.nix
+++ sets/fpga.nix
@@ -16,11 +16,5 @@
     tinyprog
     wishbone-tool
     ( callPackage ../externals/packages/fujproj { } )
-
-    # CPU stuff
-    pkgsCross.riscv64-embedded.buildPackages.binutils
-    pkgsCross.riscv64-embedded.buildPackages.gcc
-    pkgsCross.riscv32-embedded.buildPackages.binutils
-    pkgsCross.riscv32-embedded.buildPackages.gcc
   ];
 }
--- system/starlight/default.nix
+++ system/starlight/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
@@ -16,6 +16,19 @@
       KERNEL=="eth*", ATTR{address}=="00:0f:53:16:15:9d", NAME="lan10g1"
   '';
 
+  networking.networkmanager.enable = lib.mkForce false;
+  networking.bridges.br0 = {
+    rstp = true;
+    interfaces = [ "lan10g0" "lan10g1" "enp4s0" ];
+  };
+  networking.interfaces.br0 = {
+    useDHCP = true;
+    ipv6.addresses = [{
+      address = "2001:470:8b04:6900:6969:1454:7749:e591";
+      prefixLength = 128;
+    }];
+  };
+
   hardware.cpu.amd.updateMicrocode = true;
   services = {
     tor = {
@@ -24,7 +37,7 @@
     };
   };
 
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" "riscv64-linux" ];
 
   environment.systemPackages = with pkgs; [
     weechat
