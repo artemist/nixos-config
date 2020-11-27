a79b9b8fe6c67c6f353e5f891098988b6a799bbf
Artemis Tosini <me@artem.ist>
2020-11-27 06:06:45 +0000

    Add artemis to video group on laptops for light

--- sets/laptop.nix
+++ sets/laptop.nix
@@ -10,6 +10,8 @@
     upower.enable = true;
   };
   programs.light.enable = true;
+  users.users.artemis.extraGroups = [ "video" ];
+
   environment.systemPackages = with pkgs; [
     config.boot.kernelPackages.cpupower
   ];
