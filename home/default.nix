a93ab349f726787996e34b1aa6f9b59fca604576
Artemis Tosini <me@artem.ist>
2021-01-12 07:00:52 +0000

    Add mpv input.conf

--- home/default.nix
+++ home/default.nix
@@ -15,7 +15,8 @@
     xdg.enable = true;
     xdg.configFile = {
       "alacritty/alacritty.yml".source = ./files/alacritty.yml;
-      "mpv/mpv.conf".source = ./files/mpv.conf;
+      "mpv/mpv.conf".source = ./files/mpv/mpv.conf;
+      "mpv/input.conf".source = ./files/mpv/input.conf;
       "mimeapps.list".source = ./files/mimeapps.list;
       "swaylock/config".text = ''
         daemonize
