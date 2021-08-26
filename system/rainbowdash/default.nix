925a106692a49ad5e71c6f807a8999395ef9669a
Artemis Tosini <me@artem.ist>
2021-08-26 22:17:13 +0000

    Add sway config for rainbwodash

--- system/rainbowdash/default.nix
+++ system/rainbowdash/default.nix
@@ -22,6 +22,31 @@
 
   boot.supportedFilesystems = [ "nfs4" ];
 
+  # Home
+  home-manager.users.artemis = {
+    wayland.windowManager.sway.config = {
+      output."eDP-1" = { mode = "3840x2400@59.994Hz"; scale = "2"; };
+      input."1386:18753:Wacom_HID_4941_Finger".map_to_output = "eDP-1";
+      input."1739:52710:DLL096D:01_06CB:CDE6_Touchpad" = { middle_emulation = "enabled"; click_method = "clickfinger"; };
+    };
+    xdg.configFile."rustybar/config.toml".text = ''
+      [[tile]]
+      type = "iwd"
+      interface = "wlan0"
+      [[tile]]
+      type = "load"
+      [[tile]]
+      type = "memory"
+      [[tile]]
+      type = "hostname"
+      [[tile]]
+      type = "battery"
+      [[tile]]
+      type = "time"
+      format = "%Y-%m-%dT%H-%S-%S"
+    '';
+  };
+
   networking.domain = "manehattan.artem.ist";
   networking.hostName = "rainbowdash";
   system.stateVersion = "20.03";
