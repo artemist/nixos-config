99adc0eb3a81e6d93e4e5a205465f08614fd6b0c
Artemis Tosini <me@artem.ist>
2021-08-26 22:17:47 +0000

    Enable RTKit when using pipewire

---
+++ sets/pipewire.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, ... }:
+
+{
+  services.pipewire = {
+    enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = pkgs.targetPlatform.system == "x86_64-linux";
+    };
+    pulse.enable = true;
+    jack.enable = true;
+  };
+  hardware.pulseaudio.enable = lib.mkForce false;
+  security.rtkit.enable = true;
+  environment.systemPackages = with pkgs; [
+    pulseaudioLight
+    qjackctl
+
+    carla
+    lsp-plugins
+  ];
+}
