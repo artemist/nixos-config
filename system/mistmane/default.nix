f249dd5fc77cbdc798999b03f05fe70a94d2a900
Artemis Tosini <me@artem.ist>
2021-08-27 19:03:50 +0000

    Get rustybar and sway customizations working on mistmane

--- system/mistmane/default.nix
+++ system/mistmane/default.nix
@@ -1,22 +1,59 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
     ./boot-config.nix
     ./hardware-configuration.nix
     ../../externals/wip-pinebook-pro/pinebook_pro.nix
-    ../../sets/wifi.nix
+    ../../sets/laptop.nix
     ../../sets/workstation.nix
   ];
 
   networking.hostName = "mistmane";
-  programs.light.enable = true;
-  system.stateVersion = "20.09";
 
   security.pam.enableEcryptfs = true;
   environment.systemPackages = with pkgs; [
     ecryptfs
     ecryptfs-helper
-    foot
   ];
+
+  home-manager.users.artemis = {
+    programs.foot = {
+      enable = true;
+      settings.main = {
+        shell = "/run/current-system/sw/bin/fish";
+        font = "Fira Code:size=6";
+      };
+    };
+    wayland.windowManager.sway.config = {
+      terminal = lib.mkForce "foot";
+      input."9610:30:HAILUCK_CO.,LTD_USB_KEYBOARD_Touchpad" = {
+        middle_emulation = "enabled";
+        click_method = "clickfinger";
+      };
+    };
+    xdg.configFile."rustybar/config.toml".text = ''
+      [[tile]]
+      type = "iwd"
+      interface = "wlan0"
+      [[tile]]
+      type = "load"
+      [[tile]]
+      type = "memory"
+      [[tile]]
+      type = "hostname"
+      [[tile]]
+      type = "battery"
+      battery = "cw2015-battery"
+      [[tile]]
+      type = "time"
+      format = "%Y-%m-%dT%H:%M:%S"
+    '';
+  };
+  services.logind = {
+    lidSwitch = lib.mkForce "lock";
+    extraConfig = lib.mkForce "HandlePowerKey=lock";
+  };
+
+  system.stateVersion = "21.11";
 }
