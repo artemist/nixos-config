3383c0fe8c19ec0d5c87818fac58e82594a77e1f
Artemis Tosini <me@artem.ist>
2021-11-29 04:20:42 +0000

    Switch to NixOS 21.11 release

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-21.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
