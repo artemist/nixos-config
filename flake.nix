6c7a11aa8081e569d92f1263ceb8db764f983c54
Artemis Tosini <me@artem.ist>
2021-12-01 00:35:26 +0000

    Use proper NixOS 21.11 channel

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-21.11";
