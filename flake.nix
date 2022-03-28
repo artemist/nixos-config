5aa5fb07a6047c2044afe77534a95aa060fe62ca
Artemis Tosini <me@artem.ist>
2022-03-28 14:31:47 +0000

    Switch back to nixos-unstable to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/staging-next";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-21.11";
