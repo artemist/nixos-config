8286ba0cbc8aa8c4deeade05efc7baea74dfd19e
Artemis Tosini <me@artem.ist>
2022-04-26 21:00:09 +0000

    Add psotgres on starlight

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -11,12 +11,12 @@
     # We have to do this as text to gaurantee it's part of the last Host * block
     userKnownHostsFile = "~/.ssh/known_hosts ${./extra_known_hosts}";
     extraConfig = ''
-            KexAlgorithms sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256
-            MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
-            Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
-            VerifyHostKeyDNS ask
-            VisualHostKey yes
-            UpdateHostKeys ask
+      KexAlgorithms sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256
+      MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+      Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+      VerifyHostKeyDNS ask
+      VisualHostKey yes
+      UpdateHostKeys ask
     '';
 
     # Most hosts are set in private
--- system/starlight/default.nix
+++ system/starlight/default.nix
@@ -5,6 +5,7 @@
     ./boot-config.nix
     ./hardware-configuration.nix
     ./scripts.nix
+    ./postgres.nix
     ../../sets/builder.nix
     ../../sets/cpu/amd.nix
     ../../sets/fpga.nix
