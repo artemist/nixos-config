930f47d13daff96925bd83b7e2f2a16f63e34342
Artemis Tosini <me@artem.ist>
2022-11-06 04:48:36 +0000

    Add swapped scrips for weird games

--- system/starlight/scripts.nix
+++ system/starlight/scripts.nix
@@ -31,5 +31,17 @@ in
       swaymsg "output DP-2 enable mode 3840x2160 scale 2 pos 1920 0"
       swaymsg "output HDMI-A-1 enable mode 3840x2160 scale 2 pos 3840 0"
     '')
+    (pkgs.writeScriptBin "swap" ''
+      ${swayPrelude}
+      swaymsg "output DP-1 enable mode 3840x2160 scale 2 pos 1920 0"
+      swaymsg "output DP-2 enable mode 3840x2160 scale 2 pos 0 0"
+      swaymsg "output HDMI-A-1 enable mode 3840x2160 scale 2 pos 3840 0"
+    '')
+    (pkgs.writeScriptBin "swapgame" ''
+      ${swayPrelude}
+      swaymsg "output DP-1 enable mode 3840x2160 scale 1 pos 0 0"
+      swaymsg "output DP-2 enable mode 3840x2160 scale 2 pos 3840 0"
+      swaymsg "output HDMI-A-1 enable mode 3840x2160 scale 2 pos 5760 0"
+    '')
   ];
 }
