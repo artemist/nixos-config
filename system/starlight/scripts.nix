fcd7bd8035fa6417e7f79aec2e378d87e8d47d20
Artemis Tosini <me@artem.ist>
2023-01-23 19:42:09 +0000

    Move work displays to right

--- system/starlight/scripts.nix
+++ system/starlight/scripts.nix
@@ -9,15 +9,15 @@ in
   home-manager.users.artemis.home.packages = [
     (pkgs.writeScriptBin "work" ''
       ${swayPrelude}
-      swaymsg "output DP-1 disable"
+      swaymsg "output DP-1 enable mode 3840x2160 scale 2 pos 0 0"
       swaymsg "output DP-2 disable"
-      swaymsg "output DP-3 enable mode 3840x2160 scale 2 pos 0 0"
+      swaymsg "output DP-3 disable"
     '')
     (pkgs.writeScriptBin "miniwork" ''
       ${swayPrelude}
-      swaymsg "output DP-1 disable"
-      swaymsg "output DP-2 enable mode 3840x2160 scale 2 pos 0 0"
-      swaymsg "output DP-3 enable mode 3840x2160 scale 2 pos 1920 0"
+      swaymsg "output DP-1 enable mode 3840x2160 scale 2 pos 0 0"
+      swaymsg "output DP-2 enable mode 3840x2160 scale 2 pos 1920 0"
+      swaymsg "output DP-3 disable"
     '')
     (pkgs.writeScriptBin "game" ''
       ${swayPrelude}
